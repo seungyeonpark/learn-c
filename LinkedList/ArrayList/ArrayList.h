11c17f8fa00cc9a813e04cfb8ddeb2a1452a241c
seungyeonpark <siaayeon@gmail.com>
2021-04-10 23:50:35 +0900

    Fix typo

--- LinkedList/ArrayList/ArrayList.h
+++ LinkedList/ArrayList/ArrayList.h
@@ -1,10 +1,10 @@
 #ifndef __ARRAY_LIST_H__
 #define __ARRAY_LIST_H__
 
-#define TRUE			1
-#define FALSE		0
+#define TRUE	1
+#define FALSE 0
 
-#define LIST_LEN	100
+#define LIST_LEN 100
 typedef int LData;
 
 typedef struct __ArrayList
